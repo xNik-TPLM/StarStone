f98d0bab92d607a4537b1bd89252c0ebb463eb34
B2SMITH <61790129+B2SMITH@users.noreply.github.com>
2020-06-03 22:32:41 +0100

    Test-Commit-1

---
+++ StarStone/Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float m_movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.LeftShift) && Input.GetKey ("w"))
+        {
+            transform.position += transform.TransformDirection (Vector3.forward) * Time.deltaTime * m_movementSpeed * 2f;
+
+        }
+        else if (Input.GetKey("w") && !Input.GetKey (KeyCode.LeftShift))
+        {
+            transform.position += transform.TransformDirection (Vector3.forward) * Time.deltaTime * m_movementSpeed;
+        }
+        else if (Input.GetKey("s"))
+        {
+            transform.position -= transform.TransformDirection (Vector3.forward) * Time.deltaTime * m_movementSpeed;
+        }
+        if (Input.GetKey ("a") && !Input.GetKey ("d"))
+        {
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * m_movementSpeed;
+        }
+        else if (Input.GetKey("d") && !Input.GetKey("a"))
+        {
+            transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * m_movementSpeed;
+        }
+    }
+}
