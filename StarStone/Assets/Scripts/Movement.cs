e92c1b28a46f15f184b6647ad7aad1cf6c3b5f24
B2SMITH <61790129+B2SMITH@users.noreply.github.com>
2020-06-04 13:37:08 +0100

    Movement
    Movement on Axis and camera movement

--- StarStone/Assets/Scripts/Movement.cs
+++ StarStone/Assets/Scripts/Movement.cs
@@ -5,36 +5,49 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float m_movementSpeed;
-
+    public float m_mouseSensitivity = 200f;
+    public Transform Player;
+    float xRotation = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey (KeyCode.LeftShift) && Input.GetKey ("w"))
-        {
-            transform.position += transform.TransformDirection (Vector3.forward) * Time.deltaTime * m_movementSpeed * 2f;
-
-        }
-        else if (Input.GetKey("w") && !Input.GetKey (KeyCode.LeftShift))
-        {
-            transform.position += transform.TransformDirection (Vector3.forward) * Time.deltaTime * m_movementSpeed;
-        }
-        else if (Input.GetKey("s"))
-        {
-            transform.position -= transform.TransformDirection (Vector3.forward) * Time.deltaTime * m_movementSpeed;
-        }
-        if (Input.GetKey ("a") && !Input.GetKey ("d"))
+        if (Input.GetKey("w") && !Input.GetKey(KeyCode.LeftShift))
         {
             transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * m_movementSpeed;
         }
-        else if (Input.GetKey("d") && !Input.GetKey("a"))
+        else if (Input.GetKey("d"))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * m_movementSpeed;
+        }
+        else if (Input.GetKey("a"))
+        {
+            transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * m_movementSpeed;
+        }
+        if (Input.GetKey("w") && !Input.GetKey("s") && Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * m_movementSpeed * 2f;
+        }
+        else if (Input.GetKey("s") && !Input.GetKey("w"))
         {
             transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * m_movementSpeed;
+        }
+        float mouseX = Input.GetAxis("Mouse X") * m_mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * m_mouseSensitivity * Time.deltaTime;
+        //xRotation -= mouseY;
+        //xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        //transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        Player.Rotate(Vector3.up * mouseX);
+        Player.Rotate(Vector3.forward * -mouseY);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 10);
         }
     }
 }
