c8cc8782246157f9f3d8adf9e84efcbb34d0f37b
xNik-TPLM <61791371+xNik-TPLM@users.noreply.github.com>
2020-06-05 21:34:36 +0100

    StarStone Intro and Enemy test dummies
    Added models of star stones. Enemy test dummies added with health bars, Reorgonisation of some files

---
+++ StarStone/Assets/Scripts/EnemyScripts/EnemyBase.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyBase : MonoBehaviour
+{
+    public float Health;
+    public float MaxHealth;
+
+    public GameObject HealthBarUI;
+    public Slider HealthBar;
+
+    void Start()
+    {
+        Health = MaxHealth;
+
+        HealthBar.value = SetHealth();
+    }
+
+    void Update()
+    {
+        HealthBar.value = SetHealth();
+
+        if(Health < MaxHealth)
+        {
+            HealthBarUI.SetActive(true);
+        }
+
+        if(Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        /*if(Health > MaxHealth)
+        {
+            HealthBarUI.SetActive(false);
+        }*/
+    }
+
+    float SetHealth()
+    {
+        return Health / MaxHealth;
+    }
+}
