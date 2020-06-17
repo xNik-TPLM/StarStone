2eb36e61ab2657ce85b3ac275f660b8432619c87
B2SMITH <61790129+B2SMITH@users.noreply.github.com>
2020-06-17 23:22:09 +0100

    MeleeUpdate

--- StarStone/Assets/Scripts/PlayerScripts/MeleeAttack.cs
+++ StarStone/Assets/Scripts/PlayerScripts/MeleeAttack.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class MeleeAttack : MonoBehaviour
 {
+    public GameObject Knife;
     Animator meleeAnimation;
+    public float MeleeDamage;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,21 +17,26 @@ public class MeleeAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKey(KeyCode.Q))
         {
             meleeAnimation.SetBool("MeleeAttack", true);
+            if (meleeAnimation.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+            {
+                meleeAnimation.SetBool("MeleeAttack", false);
+            }
         }
-        else if (Input.GetKeyUp(KeyCode.Q))
+        //else if (Input.GetKeyUp(KeyCode.Q))
+        //{
+        //    meleeAnimation.SetBool("MeleeAttack", false);
+        //}
+    }
+    
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Enemy"))
         {
-            meleeAnimation.SetBool("MeleeAttack", false);
-        }
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            meleeAnimation.SetBool("MeleeAttackHorizontal", true);
-        }
-        else if (Input.GetKeyUp(KeyCode.R))
-        {
-            meleeAnimation.SetBool("MeleeAttackHorizontal", false);
+            Debug.Log("Works");
+            collision.gameObject.GetComponent<EnemyBase>().EnemyDamaged(MeleeDamage);
         }
     }
 }
