1266964fad81bd8a46e132b4af668f2bc1cfa0ea
B2SMITH <61790129+B2SMITH@users.noreply.github.com>
2020-07-23 12:27:08 +0100

    Menu Update

---
+++ StarStone/Assets/Scripts/MenuScripts/SFXController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXController : MonoBehaviour
+{
+    public AudioSource Walk; // This sets the reference to the audio
+    private float sfxVolume = 0.8f; // This sets a starting volume
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Walk = GetComponent<AudioSource>(); // This allows the theme to be access in the game
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Walk.volume = sfxVolume;
+    }
+
+    public void SetVolume(float vol)
+    {
+        sfxVolume = vol; // This links the slider value to the music volume
+    }
+}
