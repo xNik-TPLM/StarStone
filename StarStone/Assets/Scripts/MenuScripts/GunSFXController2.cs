57c4e87c9313facb029b438a09a5145fd7cf5e7c
B2SMITH <61790129+B2SMITH@users.noreply.github.com>
2020-07-23 13:03:56 +0100

    Slider Fix

---
+++ StarStone/Assets/Scripts/MenuScripts/GunSFXController2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunSFXController2 : MonoBehaviour
+{
+    public AudioSource Reload; // This sets the reference to the audio
+    private float reloadVolume = 0.8f; // This sets a starting volume
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Reload = GetComponent<AudioSource>(); // This allows the theme to be access in the game
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Reload.volume = reloadVolume;
+    }
+
+    public void SetVolume(float reloadvol)
+    {
+        reloadVolume = reloadvol; // This links the slider value to the music volume
+    }
+}
---
+++ StarStone/Assets/Scripts/MenuScripts/PrototypeSFXController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrototypeSFXController : MonoBehaviour
+{
+    public AudioSource Explosion; // This sets the reference to the audio
+    private float explosionVolume = 0.8f; // This sets a starting volume
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Explosion = GetComponent<AudioSource>(); // This allows the theme to be access in the game
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Explosion.volume = explosionVolume;
+    }
+
+    public void SetVolume(float explosionvol)
+    {
+        explosionVolume = explosionvol; // This links the slider value to the music volume
+    }
+}
